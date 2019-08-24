72205067ccbe03aa3c68c332051dc9d2d9df62a8
MARCELO FRANCISCONI LUBANCO THOMÉ <lubanco@edu.unifil.br>
2019-08-24 00:46:19 -0300

    Implementa o algoritimo de de média do intervalor, refinamento e critério de parada

--- src/main/java/com/calculonumerico/calculonumerico/model/EntradaFuncao.java
+++ src/main/java/com/calculonumerico/calculonumerico/model/EntradaFuncao.java
@@ -2,47 +2,19 @@ package com.calculonumerico.calculonumerico.model;
 
 public class EntradaFuncao {
 
-    private Float entradaUm;
-    private Float entradaDois;
-    private Float entradaTres;
-    private Float entradaQuatro;
-    private Float entradaCinco;
-    private Float entradaSeis;
-    private Float entradaEpsilon;
+    private Double entradaUm;
+    private Double entradaDois;
+    private Double entradaTres;
+    private Double entradaQuatro;
+    private Double entradaCinco;
+    private Double entradaSeis;
+    private Double entradaEpsilon;
 
-    public Double gerarFuncao(EntradaFuncao entradaFuncao, Double x) {
-        Double a = null;
-        Double b = null;
-        Double c = null;
-        Double d = null;
-        Double e = null;
-        Double f = null;
-        if(entradaFuncao.getEntradaSeis() != null) {
-           a = entradaFuncao.getEntradaSeis() * (Math.pow(x , 5));
-        }
-        if(entradaFuncao.getEntradaCinco() != null) {
-            b = entradaFuncao.getEntradaCinco() * (Math.pow(x , 5));
-        }
-        if(entradaFuncao.getEntradaQuatro() != null) {
-            c = entradaFuncao.getEntradaQuatro() * (Math.pow(x , 5));
-        }
-        if(entradaFuncao.getEntradaTres() != null) {
-            d = entradaFuncao.getEntradaTres() * (Math.pow(x , 5));
-        }
-        if(entradaFuncao.getEntradaDois() != null) {
-            e = entradaFuncao.getEntradaDois() * (Math.pow(x , 5));
-        }
-        if(entradaFuncao.getEntradaUm() != null) {
-            f = entradaFuncao.getEntradaUm() * (Math.pow(x , 5));
-        }
-
-        return a + b + c + d + e + f;
-    }
 
     public EntradaFuncao() {
     }
 
-    public EntradaFuncao(Float entradaUm, Float entradaDois, Float entradaTres, Float entradaQuatro, Float entradaCinco, Float entradaSeis, Float entradaEpsilon) {
+    public EntradaFuncao(Double entradaUm, Double entradaDois, Double entradaTres, Double entradaQuatro, Double entradaCinco, Double entradaSeis, Double entradaEpsilon) {
         this.entradaUm = entradaUm;
         this.entradaDois = entradaDois;
         this.entradaTres = entradaTres;
@@ -52,59 +24,59 @@ public class EntradaFuncao {
         this.entradaEpsilon = entradaEpsilon;
     }
 
-    public Float getEntradaUm() {
+    public Double getEntradaUm() {
         return entradaUm;
     }
 
-    public void setEntradaUm(Float entradaUm) {
+    public void setEntradaUm(Double entradaUm) {
         this.entradaUm = entradaUm;
     }
 
-    public Float getEntradaDois() {
+    public Double getEntradaDois() {
         return entradaDois;
     }
 
-    public void setEntradaDois(Float entradaDois) {
+    public void setEntradaDois(Double entradaDois) {
         this.entradaDois = entradaDois;
     }
 
-    public Float getEntradaTres() {
+    public Double getEntradaTres() {
         return entradaTres;
     }
 
-    public void setEntradaTres(Float entradaTres) {
+    public void setEntradaTres(Double entradaTres) {
         this.entradaTres = entradaTres;
     }
 
-    public Float getEntradaQuatro() {
+    public Double getEntradaQuatro() {
         return entradaQuatro;
     }
 
-    public void setEntradaQuatro(Float entradaQuatro) {
+    public void setEntradaQuatro(Double entradaQuatro) {
         this.entradaQuatro = entradaQuatro;
     }
 
-    public Float getEntradaCinco() {
+    public Double getEntradaCinco() {
         return entradaCinco;
     }
 
-    public void setEntradaCinco(Float entradaCinco) {
+    public void setEntradaCinco(Double entradaCinco) {
         this.entradaCinco = entradaCinco;
     }
 
-    public Float getEntradaSeis() {
+    public Double getEntradaSeis() {
         return entradaSeis;
     }
 
-    public void setEntradaSeis(Float entradaSeis) {
+    public void setEntradaSeis(Double entradaSeis) {
         this.entradaSeis = entradaSeis;
     }
 
-    public Float getEntradaEpsilon() {
+    public Double getEntradaEpsilon() {
         return entradaEpsilon;
     }
 
-    public void setEntradaEpsilon(Float entradaEpsilon) {
+    public void setEntradaEpsilon(Double entradaEpsilon) {
         this.entradaEpsilon = entradaEpsilon;
     }
 }
--- src/main/java/com/calculonumerico/calculonumerico/service/CalculoService.java
+++ src/main/java/com/calculonumerico/calculonumerico/service/CalculoService.java
@@ -3,26 +3,139 @@ package com.calculonumerico.calculonumerico.service;
 import com.calculonumerico.calculonumerico.model.EntradaFuncao;
 import com.calculonumerico.calculonumerico.model.Funcao;
 import com.calculonumerico.calculonumerico.model.Intervalo;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CalculoService {
+
+    private List<Double> raizesDaFuncao = new ArrayList<>();
 
     public void calcularMetodoBissecaoNaoLinear(EntradaFuncao entradaFuncao) {
         System.out.println("FUNCIONANDO");
     }
 
-    //TODO refatorar para receber uma classe Intevarlo (Double a, Double b)
+    /**
+     * Calcula a média do intervalo
+     * @param intervalo
+     * @return
+     */
     public Double calcularMediaDeIntervalo(Intervalo intervalo) {
         return (intervalo.getA() + intervalo.getB()) / 2;
     }
 
 
+    /**
+     * Gerar um novo intervalo
+     * @param a
+     * @param b
+     * @return
+     */
     public Intervalo gerarIntervalo(Double a, Double b) {
         return new Intervalo(a, b);
     }
 
-    public void analisarIntervaloNaFuncao(Intervalo intervalo, Funcao funcao) {
+    @Scheduled(fixedDelay = 500000)
+    public void testeFuncao() {
+        Intervalo intervalo = new Intervalo(0.5, 1.0);
+        EntradaFuncao entradaFuncao = new EntradaFuncao(5.0,1.0,3.0,null,null,null,0.01);
+        Double mediaDeIntervalo = calcularMediaDeIntervalo(intervalo);
+        refinarIntervalo(entradaFuncao, intervalo, mediaDeIntervalo);
+        System.out.println("TESTE");
+    }
 
+    /**
+     * É responsável por gerar a função.
+     * @param entradaFuncao é o input passado pelo usuário no frontend
+     * @param x é o valor a ser calculado, na maioria das vezes vaiser a média do intervalo e
+     *          no começo os valores inciais do array principal
+     * @return
+     */
+    public Double calcularFuncao(EntradaFuncao entradaFuncao, Double x) {
+        Double a = 0.0;
+        Double b = 0.0;
+        Double c = 0.0;
+        Double d = 0.0;
+        Double e = 0.0;
+        Double f = 0.0;
+        if(entradaFuncao.getEntradaSeis() != null) {
+            a = entradaFuncao.getEntradaSeis() * (Math.pow(x , 5));
+        }
+        if(entradaFuncao.getEntradaCinco() != null) {
+            b = entradaFuncao.getEntradaCinco() * (Math.pow(x , 4));
+        }
+        if(entradaFuncao.getEntradaQuatro() != null) {
+            c = entradaFuncao.getEntradaQuatro() * (Math.pow(x , 3));
+        }
+        if(entradaFuncao.getEntradaTres() != null) {
+            d = entradaFuncao.getEntradaTres() * (Math.pow(x , 2));
+        }
+        if(entradaFuncao.getEntradaDois() != null) {
+            e = entradaFuncao.getEntradaDois() * x;
+        }
+        if(entradaFuncao.getEntradaUm() != null) {
+            f = entradaFuncao.getEntradaUm();
+        }
+        Double resultado = a + b + c + d + e + f;
+        System.out.println("O resultado da funcao é: " + resultado);
+        return resultado;
+    }
+
+
+    /**
+     * É responsável por refinar os intervalos até encontrar o critério de parada e recuperar
+     * as raízes reais.
+     *
+     * @param entradaFuncao
+     * @param intervalo
+     * @param mediaIntervalo
+     */
+    public void refinarIntervalo(EntradaFuncao entradaFuncao, Intervalo intervalo, Double mediaIntervalo) {
+       while (isCriterioDeParada(intervalo, entradaFuncao.getEntradaEpsilon())) {
+           System.out.println("O critério ainda não pe verdadeiro");
+           Double resultadoFuncao = calcularFuncao(entradaFuncao, mediaIntervalo);
+           System.out.println("Resultado da raiz da funcao: " + resultadoFuncao);
+           if (resultadoFuncao > 0) {
+               System.out.println("É positivo");
+               intervalo.setB(mediaIntervalo);
+           } else {
+               System.out.println("É negativo");
+               intervalo.setA(mediaIntervalo);
+           }
+           raizesDaFuncao.add(resultadoFuncao);
+           mediaIntervalo = calcularMediaDeIntervalo(intervalo);
+
+       }
+       System.out.println("Finalização do calculo de raízes reais: " + raizesDaFuncao.get(raizesDaFuncao.size() -1));
+//        return intervalo;
+    }
+
+    /**
+     * Valida se o critério de parada já foi atendido, caso não continue refinando os intervalos
+     *
+     * @param intervalo
+     * @param epsilon
+     * @return
+     */
+    public boolean isCriterioDeParada(Intervalo intervalo, Double epsilon) {
+        Double epsilonCalculado = epsilon; //calcularEpsilon(epsilon);
+        Double modulo = intervalo.getB() - intervalo.getA();
+        if (modulo < epsilonCalculado) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Calcula o valor do epsilon
+     * @param epsilon
+     * @return
+     */
+    public Double calcularEpsilon(Double epsilon) {
+        return Math.pow(10, epsilon);
     }
 }
